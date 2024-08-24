970c9261373eba31c16c0abbcf256e37e0b27964
andrewliew86 <andrewliew86@yahoo.com.au>
2024-08-24 15:17:15 +1000

    Customized handler.py and added sd_model.py file for Stable Diffusion model

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,7 @@
 """ Example handler file. """
 
 import runpod
+from sd_model import generate_image_bytes
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
@@ -8,11 +9,8 @@ import runpod
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
-    job_input = job['input']
+    prompt = job["input"]["prompt"]
 
-    name = job_input.get('name', 'World')
-
-    return f"Hello, {name}!"
-
+    return generate_image_bytes(prompt)
 
 runpod.serverless.start({"handler": handler})
