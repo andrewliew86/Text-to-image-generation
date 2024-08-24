34c5fc983996dbedb360e6cd3e092103378d0a48
andrewliew86 <andrewliew86@yahoo.com.au>
2024-08-24 20:43:50 +1000

    Added clarifying comments for endpoint ID

--- image-generation-prompt.py
+++ image-generation-prompt.py
@@ -11,7 +11,10 @@ from dotenv import load_dotenv
 # Load env file
 load_dotenv()
 
+# Add RunPod API key
 runpod.api_key = os.environ.get("RUNPOD_API_KEY")
+
+# Change this to your unique RunPod serverless endpoint ID 
 endpoint = runpod.Endpoint("wogtprlnbnmng5")
 
 prompt = "Photograph taken in 1883, old west, cowboys"
